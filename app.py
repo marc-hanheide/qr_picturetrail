b6726119d4d84d9a7042e45b561993b1cf8b96e9
Marc Hanheide <marc@hanheide.net>
2024-09-02 20:02:29 +0000

    WIP

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ https://github.com/petersimeth/basic-flask-template
 © MIT licensed, 2018-2023
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 DEVELOPMENT_ENV = True
 
@@ -19,6 +19,16 @@ app_data = {
     "html_title": "QR Picture Trail",
     "project_name": "QR_PictureTrail",
     "keywords": "qr, trail, kids, flask, webapp",
+    "id_dict": {
+        "9509af0a-1764-4095-aa5c-20726b654146": "lighthouse",
+        "ce888ab1-ac19-40da-9b98-ebd65742d050": "lightning",
+        "6e9ffaa0-688a-4a9e-8126-2d9c34596733": "lightbulb",
+        "e29e6efd-4dcc-4fda-bebe-65657a66d6ad": "fireworks",
+        "872055cd-4566-4403-a051-6fae3466f9d0": "candle",
+        "d4b77f9b-446e-4898-982c-ca05109b2b3f": "torch",
+        "18c5588a-11ce-4c74-88fd-1bbcf153f08f": "campfire",
+        "250279a7-444e-4bed-9f7f-cd5d2eb8200d": "shootingstar"
+    }
 }
 
 
@@ -32,9 +42,14 @@ def about():
     return render_template("about.html", app_data=app_data)
 
 
-@app.route("/service")
-def service():
-    return render_template("service.html", app_data=app_data)
+@app.route("/trail")
+def trail():
+    id = request.args.get("id", default=0, type=str)
+    if id in app_data["id_dict"]:
+        return render_template("trail.html", app_data=app_data, found=id)
+    else:
+        return render_template("notfound.html", app_data=app_data, found=None)
+
 
 
 @app.route("/contact")
