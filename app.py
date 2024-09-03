5d843d1dc7d540f4d31d7e3db306f284a4411cee
Marc Hanheide <marc@hanheide.net>
2024-09-03 17:03:50 +0100

    all imaged

--- app.py
+++ app.py
@@ -10,30 +10,12 @@ https://github.com/petersimeth/basic-flask-template
 from flask import Flask, render_template, request, send_file, session
 from uuid import uuid4
 
+from config import app_data
+
 DEVELOPMENT_ENV = True
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 app.secret_key = uuid4().bytes
-
-app_data = {
-    "name": "QR Picture Trail",
-    "base_url": "http://127.0.0.1:5000/",
-    "description": "A basic Flask app using bootstrap to implement a QR-based picture trail.",
-    "author": "Marc Hanheide",
-    "html_title": "QR Picture Trail",
-    "project_name": "QR_PictureTrail",
-    "keywords": "qr, trail, kids, flask, webapp",
-    "id_dict": {
-        "9509af0a-1764-4095-aa5c-20726b654146": "lighthouse",
-        "ce888ab1-ac19-40da-9b98-ebd65742d050": "lightning",
-        "6e9ffaa0-688a-4a9e-8126-2d9c34596733": "lightbulb",
-        "e29e6efd-4dcc-4fda-bebe-65657a66d6ad": "fireworks",
-        "872055cd-4566-4403-a051-6fae3466f9d0": "candle",
-        "d4b77f9b-446e-4898-982c-ca05109b2b3f": "torch",
-        "18c5588a-11ce-4c74-88fd-1bbcf153f08f": "campfire",
-        "250279a7-444e-4bed-9f7f-cd5d2eb8200d": "shootingstar"
-    }
-}
 
 
 @app.route("/")
@@ -46,20 +28,22 @@ def index():
 def about():
     return render_template("about.html", app_data=app_data)
 
+def initialise_session():
+    for item in app_data["id_dict"]:
+        found_id = 'found_'+item
+        if found_id not in session:
+            session[found_id] = False
 
 @app.route("/trail")
 def trail():
     id = request.args.get("id", default=0, type=str)
-    if 'found_ids' not in session:
-        print('initialise')
-        session['found_ids'] = []
     if id in app_data["id_dict"]:
-        print('add ' + id + " to found_ids, which previously was:" + str(session['found_ids']))
-        session['found_ids'].append(id)
-        print(session['found_ids'])
-        return render_template("trail.html", app_data=app_data, found=id, found_ids=session['found_ids'])
+        initialise_session()
+        found_id = 'found_'+id
+        session[found_id] = True
+        return render_template("trail.html", app_data=app_data, found=id)
     else:
-        return render_template("trail.html", app_data=app_data, found=None, found_ids=session['found_ids'])
+        return render_template("trail.html", app_data=app_data, found=None)
 
 
 
---
+++ config.py
@@ -0,0 +1,19 @@
+app_data = {
+    "name": "QR Picture Trail",
+    "base_url": "http://127.0.0.1:5000/",
+    "description": "A basic Flask app using bootstrap to implement a QR-based picture trail.",
+    "author": "Marc Hanheide",
+    "html_title": "QR Picture Trail",
+    "project_name": "QR_PictureTrail",
+    "keywords": "qr, trail, kids, flask, webapp",
+    "id_dict": {
+        "9509af0a-1764-4095-aa5c-20726b654146": "lighthouse",
+        "ce888ab1-ac19-40da-9b98-ebd65742d050": "lightning",
+        "6e9ffaa0-688a-4a9e-8126-2d9c34596733": "lightbulb",
+        "e29e6efd-4dcc-4fda-bebe-65657a66d6ad": "fireworks",
+        "872055cd-4566-4403-a051-6fae3466f9d0": "candle",
+        "d4b77f9b-446e-4898-982c-ca05109b2b3f": "torch",
+        "18c5588a-11ce-4c74-88fd-1bbcf153f08f": "campfire",
+        "250279a7-444e-4bed-9f7f-cd5d2eb8200d": "shootingstar"
+    }
+}
