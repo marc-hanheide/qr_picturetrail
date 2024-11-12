aa2fc220c64b9f62013db7ced4a49a686ccfd9e7
Marc Hanheide <marc@hanheide.net>
2024-11-12 08:46:11 +0000

    minor text corrections

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 app_data = {
-    "name": "WEL QR Picture Trail",
+    "name": "West End Lights Interactive Trail",
     "description": "Find all our light-themed posters on the WEL trail",
     "author": "Marc Hanheide",
     "html_title": "WEL QR Picture Trail",
@@ -9,37 +9,44 @@ app_data = {
         "9509af0a-1764-4095-aa5c-20726b654146": {
             "image": "sun",
             "title": "The Sun",
-            "text": "The sun is a giant ball of burning gas in space. It's so hot that it makes its own light, which travels all the way to Earth to give us sunshine! That's how we can see during the day and feel warm."
+            "text": "The sun is a giant ball of burning gas in space. It's so hot that it makes its own light, which travels all the way to Earth to give us sunshine! That's how we can see during the day and feel warm.",
+            "hint": "I'm the Queen of all lights, so I'm in a street named accordingly."
         },
         "ce888ab1-ac19-40da-9b98-ebd65742d050": {
             "image": "fire",
             "title": "Fire",
-            "text": "When things burn, they make light and heat. A campfire is made of wood burning, and the flames give us light to see in the dark and keep us cosy!"
+            "text": "When things burn, they make light and heat. A campfire is made of wood burning, and the flames give us light to see in the dark and keep us cosy!",
+            "hint": "Where would you find a cosy fire to have a drink at in the West End?"
         },
         "6e9ffaa0-688a-4a9e-8126-2d9c34596733": {
             "image": "lightbulb",
             "title": "Incandescent Light Bulb",
-            "text": "Inside a light bulb is a tiny wire. When you turn on the light, electricity flows through the wire and makes it get so hot that it glows really bright!"
+            "text": "Inside a light bulb is a tiny wire. When you turn on the light, electricity flows through the wire and makes it get so hot that it glows really bright!",
+            "hint": "Shine a light on (junior) education!"
         },
         "e29e6efd-4dcc-4fda-bebe-65657a66d6ad": {
             "image": "fireflies",
             "title": "Fireflies (Bioluminescence)",
-            "text": "Fireflies are tiny insects that have their own lights inside their bodies. They use their lights to talk to each other and find friends in the dark!"
+            "text": "Fireflies are tiny insects that have their own lights inside their bodies. They use their lights to talk to each other and find friends in the dark!",
+            "hint": "I can be found near a place that offers wrap around care and a breakfast club with my name."
         },
         "872055cd-4566-4403-a051-6fae3466f9d0": {
             "image": "moon",
             "title": "The Moon",
-            "text": "The moon doesn't make its own light. It's like a giant mirror that reflects the light from the sun. That's why it shines at night!"
+            "text": "The moon doesn't make its own light. It's like a giant mirror that reflects the light from the sun. That's why it shines at night!",
+            "hint": "Find me near a local cafe/restaurant in the <i>west</i> West-End."
         },
         "d4b77f9b-446e-4898-982c-ca05109b2b3f": {
             "image": "lightning",
             "title": "Lightning",
-            "text": "Lightning is a flash of light that happens in the sky during a thunderstorm. It's really, really bright and can make a loud bang! Lightning is made when electricity builds up in clouds and then jumps to the ground or to another cloud."
+            "text": "Lightning is a flash of light that happens in the sky during a thunderstorm. It's really, really bright and can make a loud bang! Lightning is made when electricity builds up in clouds and then jumps to the ground or to another cloud.",
+            "hint": "Find me at a very COMMON entrance."
         },
         "18c5588a-11ce-4c74-88fd-1bbcf153f08f": {
             "image": "led",
             "title": "LEDs (Light Emitting Diodes)",
-            "text": "LEDs are tiny lights that are used in lots of things, like TVs, computers, and even Christmas lights! LEDs are very energy-efficient, which means they use less electricity than other types of lights. They also last for a long time, so you don't have to change them as often."
+            "text": "LEDs are tiny lights that are used in lots of things, like TVs, computers, and even Christmas lights! LEDs are very energy-efficient, which means they use less electricity than other types of lights. They also last for a long time, so you don't have to change them as often.",
+            "hint": "I'm where you can find horses during the day, and NOT where \"lightning\" is."
         }
     }
 }
